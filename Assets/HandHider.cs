8c0eddea2046baadd0b83be46c241f97a65a24e6
Sagitarian812 <62075331+Sagitarian812@users.noreply.github.com>
2020-11-08 23:52:10 -0500

    VR lab 3

---
+++ Assets/HandHider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class HandHider : MonoBehaviour
+{
+    private SkinnedMeshRenderer meshRenderer = null;
+    private XRDirectInteractor interactor = null;
+
+    void Awake()
+    {
+        meshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        interactor = GetComponent<XRDirectInteractor>();
+
+        interactor.onHoverEnter.AddListener(Hide);
+        interactor.onHoverExit.AddListener(Show);
+    }
+
+    private void OnDestroy()
+    {
+        interactor.onHoverEnter.RemoveListener(Hide);
+        interactor.onHoverExit.RemoveListener(Show);
+    }
+
+    private void Show(XRBaseInteractable interactable)
+    {
+        meshRenderer.enabled = true;
+    }
+
+    private void Hide(XRBaseInteractable interactable)
+    {
+        meshRenderer.enabled = false;
+    }
+}
---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class OpenDoor : MonoBehaviour
+{
+    public Transform door;
+
+    private void Start()
+    {
+        door = gameObject.GetComponent<Transform>();
+    }
+
+    public void openDoor()
+    {
+        door.position = new Vector3(5.54f, -2.55f, -0.54f);
+    }
+    public void closeDoor()
+    {
+        door.position = new Vector3(5.54f, 1.75f, -0.54f);
+    }
+}
